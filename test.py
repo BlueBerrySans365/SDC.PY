19f29c4d355128e3992b197830130e6c4fa169f2
Nick Salt <46006335+BlueBerrySans365@users.noreply.github.com>
2021-03-01 14:34:28 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import nsfkit
-import json
-
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc4MDg3NzQ5MDAxNjIyMzIzMyIsInBlcm1zIjowLCJpYXQiOjE2MTA0NTIxNTF9.z7KY-CW_6vIMknsPupodkeVPT7PzooSAoladP4PyrGA"
-
-test = nsfkit.SD().getRated(DSID="752114880588283964", BDSToken=token)
-
-print(test)
